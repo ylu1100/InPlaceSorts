9437152a9d53c695b4540c88bbc806716e940013
ylu1100 <43207435+ylu1100@users.noreply.github.com>
2019-01-11 08:36:31 -0500

    Update SortingUtil.java

--- src/com/company/SortingUtil.java
+++ src/com/company/SortingUtil.java
@@ -37,6 +37,17 @@ public class SortingUtil {
         }
         return index;
     }
+    public static boolean checkSum(double[] before, double[] after) {
+        double sumbefore = 0;
+        double sumafter = 0;
+        for (int x = 0; x < before.length; x++) {
+            sumbefore = sumbefore + before[x];
+        }
+        for (int y = 0; y <after.length; y++) {
+            sumafter = sumafter + after[y];
+        }
+        return sumafter == sumbefore;
+    }
     public static boolean checkSum(int[] before, int[] after) {
         int sumbefore = 0;
         int sumafter = 0;
@@ -48,6 +59,14 @@ public class SortingUtil {
         }
         return sumafter == sumbefore;
     }
+    public static double[]copydoublearray(double[]arr){
+        double[]copyarray=new double [arr.length];{
+            for(int i = 0;i<copyarray.length;i++){
+                copyarray[i]=arr[i];
+            }
+            return copyarray;
+        }
+    }
     public static int[]copyintarray(int[]arr){
         int[]copyarray=new int [arr.length];{
             for(int i = 0;i<copyarray.length;i++){
@@ -56,6 +75,22 @@ public class SortingUtil {
             return copyarray;
         }
     }
+    public static boolean isSorted(String[] testArr) {
+        for (int x = 0; x < testArr.length - 1; x++) {
+            if (testArr[x].compareTo(testArr[x + 1])>1) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean isSorted(double[] testArr) {
+        for (int x = 0; x < testArr.length - 1; x++) {
+            if (testArr[x] > testArr[x + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
     public static boolean isSorted(int[] testArr) {
         for (int x = 0; x < testArr.length - 1; x++) {
             if (testArr[x] > testArr[x + 1]) {
@@ -63,5 +98,34 @@ public class SortingUtil {
             }
         }
         return true;
+    }
+    public static String[]randomStringArr(int count,int length){
+        String[]arr=new String[count];
+       while(count>0){
+           int i=0;
+           String s = "";
+           while(i<length){
+               char c = (char)((Math.random()*26)+97);
+               s=s+c;
+               i++;
+           }
+           count--;
+           arr[count]=s;
+       }
+       return arr;
+    }
+    public static double[] randomDoubleArr(int count) {
+        double[] arr = new double[count];
+        for (int counter = 0; counter < count; counter++) {
+            arr[counter] = (Math.random() * 10000);
+        }
+        return arr;
+    }
+    public static int[] randomIntArr(int count) {
+        int[] arr = new int[count];
+        for (int counter = 0; counter < count; counter++) {
+            arr[counter] = (int) (Math.random() * 10000);
+        }
+        return arr;
     }
 }
