06eb921312e78bb32ce083600d5ca82cd2b2a4a3
ylu1100 <43207435+ylu1100@users.noreply.github.com>
2019-01-14 08:33:00 -0500

    Update Runner.java

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -6,19 +6,21 @@ public class Runner {
         String[]arr = SortingUtil.randomStringArr(8,5);
         double[]arr2 = SortingUtil.randomDoubleArr(8);
         int[]arr3 = SortingUtil.randomIntArr(8);
+        long timeStart=0;
+        long timeStop=0;
         System.out.println("BubbleSort");
         for(String num:arr){
             System.out.print(num+" ");
         }
         System.out.println();
-        long time = System.nanoTime();
+        timeStart = System.nanoTime();
         InPlaceSorts.bubbleSort(arr);
-        time = System.nanoTime()-time;
+        timeStop = System.nanoTime()-timeStart;
         for(String num:arr){
             System.out.print(num+" ");// write your code here
         }
         System.out.println();
-        System.out.println("Time Taken: "+time);
+        System.out.println("Time Taken: "+timeStop);
         System.out.println("Is it sorted?: "+SortingUtil.isSorted(arr));
         System.out.println();
         System.out.println("SelectionSort");
@@ -27,15 +29,15 @@ public class Runner {
         }
         System.out.println();
         double[]before2 = SortingUtil.copydoublearray(arr2);
-        long time2 = System.nanoTime();
+        timeStart = System.nanoTime();
         InPlaceSorts.selectionSort(arr2);
-        time2 = System.nanoTime()-time2;
+        timeStop = System.nanoTime()-timeStart;
         double[]after2 = SortingUtil.copydoublearray(arr2);
         for(double num:arr2){
             System.out.print(num+" ");
         }
         System.out.println();
-        System.out.println("Time Taken: "+time2);
+        System.out.println("Time Taken: "+timeStop);
         System.out.println("Are the sums equal?: "+SortingUtil.checkSum(before2,after2));
         System.out.println("Is it sorted?: "+SortingUtil.isSorted(arr2));
         System.out.println();
@@ -45,15 +47,15 @@ public class Runner {
         }
         System.out.println();
         int[] before3 = SortingUtil.copyintarray(arr3);
-        long time3 = System.nanoTime();
+        timeStart = System.nanoTime();
         InPlaceSorts.insertionSort(arr3);
-        time3 = System.nanoTime()-time3;
+        timeStop = System.nanoTime()-timeStart;
         int[]after3=SortingUtil.copyintarray(arr3);
         for(int num:arr3){
             System.out.print(num+" ");
         }
         System.out.println();
-        System.out.println("Time Taken: "+time3);
+        System.out.println("Time Taken: "+timeStop);
         System.out.println("Are the sums equal?: "+SortingUtil.checkSum(before3,after3));
         System.out.println("Is it sorted?: "+SortingUtil.isSorted(arr3));
     }
